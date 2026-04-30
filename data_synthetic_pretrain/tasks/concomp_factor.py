4a9140fb13f067876b6879dbf71024b790f36b63
Kirill456Z <Kirill456Z@yandex.ru>
2026-04-30 14:20:37 +0200

    Version 0.4.16

--- data_synthetic_pretrain/tasks/concomp_factor.py
+++ data_synthetic_pretrain/tasks/concomp_factor.py
@@ -47,7 +47,8 @@ class ConCompFactorTaskGenerator(BaseSynteticTaskGenerator):
         return cls(ConCompFactorGenerationArgs.model_validate(config))
 
     def max_generation_length(self):
-        return self.config.graph_generator_config.max_token_length * self.config.max_nodes + 1
+        max_comps = self.config.graph_generator_config.max_connectivity_components or self.config.max_nodes
+        return self.config.graph_generator_config.max_token_length * max_comps + 1
 
     def _sample_num_nodes(self):
         node_choices = list(range(3, self.config.max_nodes + 1))
